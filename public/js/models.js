cfbb497d2ea244406b3ec334298014d2f9813bd3
anapsn <anapaulanovais25@gmail.com>
2023-06-25 14:29:20 -0300

    Atualização cardaio e cores

---
+++ public/js/models.js
@@ -0,0 +1,10 @@
+let modelsJson = [
+    {id:1, name:'Sushi Tradicional', img:'css/assents/img/sushipng.png', price:[30, 60, 90], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:2, name:'Sashimi Camarão', img:'css/assents/img/2.png', price:[20, 40, 60], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:3, name:'Barca', img:'css/assents/img/3.png', price:[80, 100, 150], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:4, name:'Caril de camarão Teppanyaki', img:'css/assents/img/4.png', price:[15, 25, 30], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:5, name:'Temaki', img:'css/assents/img/5.png', price:[15, 15, 15], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:6, name:'Yakisoba', img:'css/assents/img/6.png', price:[14.00, 22.00, 29.00], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:7, name:'Salmão Simples', img:'css/assents/img/7.png', price:[20.00, 30.00, 40.00], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'},
+    {id:8, name:'Umaraki', img:'css/assents/img/8.png', price:[18, 24.00, 30.00], sizes:['1/72', '1/48', '1/32'], description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'}
+];
